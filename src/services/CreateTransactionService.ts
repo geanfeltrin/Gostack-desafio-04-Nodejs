97b11c115e39862fe1e1f6266118b71823f05d7d
Gean Feltrin <gean_costa@hotmail.com>
2020-07-05 19:27:26 -0300

    Create Transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import CreateCategoryService from './CreateCategoryService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    const categoryService = new CreateCategoryService();
+    const categoryResult = await categoryService.execute({
+      title: category,
+    });
+
+    const transaction = transactionRepository.create({
+      type,
+      title,
+      value,
+      category: categoryResult,
+    });
+
+    if (!transaction) {
+      throw new AppError('Can not possible create transaction');
+    }
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
